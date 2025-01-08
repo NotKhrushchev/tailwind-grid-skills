48386386b5942072e3c0eca682a149c949228de4
Nikita Ipatov <nikita@Nikitas-MacBook-Air.local>
2025-01-08 19:45:50 +0300

    added optimal layout depending on device screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ export default {
                 sans: ['Barlow Semi Condensed', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            white: '#ffffff',
+            violet: {
+                light: 'hsl(263,55%,70%)',
+                DEFAULT: 'hsl(263,55%,52%)',
+            },
+            gray: {
+                light: 'hsl(217,19%,70%)',
+                DEFAULT: 'hsl(217,19%,35%)',
+            },
+            blue: {
+                light: 'hsl(219,29%,70%)',
+                DEFAULT: 'hsl(219,29%,14%)',
+            },
+        },
     },
     plugins: [],
 };
